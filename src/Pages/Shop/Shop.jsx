e3195a944ad40989e6ccd7e11845fe2de8cbff4b
Conrad Obi <helpdeskkng@gmail.com>
2023-11-10 10:21:16 +0100

    removed_wrongbranch

---
+++ src/Pages/Shop/Shop.jsx
@@ -0,0 +1,13 @@
+// import "./Shop.css";
+// import Filter from "./Filter";
+// import ShopList from "./ShopList";
+const Shop = () => {
+  return (
+    <>
+      {/* <Filter />
+      <ShopList /> */}
+    </>
+  );
+};
+
+export default Shop;
