71a662340918c75ee5df3e8d7208909a552da76d
Conrad Obi <helpdeskkng@gmail.com>
2023-11-13 12:05:38 +0100

    fixed_issues

--- src/Pages/Blog/Pagination.jsx
+++ src/Pages/Blog/Pagination.jsx
@@ -2,7 +2,7 @@ const Pagination = () => {
   return (
     <>
       <div className="pagination">
-        <button>1</button>
+        <button className="active">1</button>
         <button>2</button>
         <button>3</button>
         <button>Next</button>
