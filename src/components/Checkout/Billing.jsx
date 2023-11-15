e193d6b2b50d0d95449969f4678a1435304e01ff
Arisheila <s.arinaitwe@student.ciu.ac.ug>
2023-11-15 22:13:31 +0300

    added checkout file

--- src/components/Checkout/Billing.jsx
+++ src/components/Checkout/Billing.jsx
@@ -1,130 +1,204 @@
-import React from 'react'
-import Divider from '@mui/material/Divider';
-import Elipse from "../../assets/contact/Ellipse 2.png";
-import Elipse2 from "../../assets/contact/Ellipse 1.png";
- import{Stack,Button,Typography, Box} from '@mui/material';
-import Radio from '@mui/material/Radio';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import React from "react"
+import Divider from "@mui/material/Divider"
+import Elipse from "../../assets/contact/Ellipse 2.png"
+import Elipse2 from "../../assets/contact/Ellipse 1.png"
+import { Stack, Button, Typography, Box } from "@mui/material"
+import Radio from "@mui/material/Radio"
+import FormControlLabel from "@mui/material/FormControlLabel"
 
 export default function Billing() {
-  return (
-    <Box>  
-            <div className="item" style={{display:"flex", gap: "40px", marginBottom:"20px"}}>
-                 <Typography variant='h5'>Product</Typography>
-                 <Typography variant='h5'>Subtotal</Typography>
-             </div>
-             <div className="item" style={{display:"flex", gap: "40px", marginBottom: "20px"}}>
-                 <p> <span>Asgaard </span> x 1</p>
-                 <Typography variant='body-2'>Rs. 250,000.00</Typography>
-             </div>
-             <div className="item" style={{display:"flex", gap: "40px", marginBottom: "20px"}}>
-                 <Typography variant='h5'>Subtotal</Typography>
-                 <Typography variant='body-2'>Rs. 250,000.00</Typography>
-             </div>
-             <div className="item" style={{display:"flex", gap: "40px", marginBottom: "20px"}}>
-                 <Typography variant='h5'>Total</Typography>
-                 <Typography variant='body-2'>Rs. 250,000.00</Typography>
-             </div>
-             <Divider />
-             <div style={{ display: "flex", gap: "10px",marginBottom: "20px" }}>
- 				<img
- 					src={Elipse}
- 					alt="logo"
- 					style={{
- 						width:"14px",
- 						height:"14px",
- 					}}
- 				/>
- 					<Typography variant='body-1'
- 						style={{
- 							fontSize: "16px",
- 							fontWeight: "400",
- 							lineHeight: " 24px",
-                            }}>
- 						{" "}
- 					Direct Bank Transfer
- 					</Typography>
-			
+	return (
+		<Box>
+			<div
+				className="item"
+				style={{ display: "flex", gap: "40px", marginBottom: "20px" }}
+			>
+				<Typography
+					sx={{
+						fontSize: "24px",
+						fontWeight: "500",
+						lineHeight: "36px",
+					}}
+				>
+					Product
+				</Typography>
+				<Typography
+					sx={{
+						fontSize: " 24px",
+						fontWeight: "500",
+						lineHeight: "36px",
+					}}
+				>
+					Subtotal
+				</Typography>
+			</div>
+			<div
+				className="item"
+				style={{ display: "flex", gap: "40px", marginBottom: "20px" }}
+			>
+				<p>
+					{" "}
+					<span
+						style={{
+							color: "#9F9F9F",
+						}}
+					>
+						Asgaard{" "}
+					</span>{" "}
+					x 1
+				</p>
+				<Typography
+					sx={{ fontWeight: "300", FontSize: "16px", lineHeight: "24px" }}
+				>
+					Rs. 250,000.00
+				</Typography>
+			</div>
+			<div
+				className="item"
+				style={{ display: "flex", gap: "40px", marginBottom: "20px" }}
+			>
+				<Typography
+					sx={{
+						fontSize: "16px",
+						fontWeight: "400",
+						lineHeight: "24px",
+					}}
+				>
+					Subtotal
+				</Typography>
+				<Typography
+					sx={{ fontWeight: "300", fontSize: "16px", lineHeight: "24px" }}
+				>
+					Rs. 250,000.00
+				</Typography>
+			</div>
+			<div
+				className="item"
+				style={{ display: "flex", gap: "40px", marginBottom: "20px" }}
+			>
+				<Typography
+					sx={{
+						fontSize: "16px",
+						fontWeight: "400",
+						lineHeight: "24px",
+					}}
+				>
+					Total
+				</Typography>
+				<Typography
+					sx={{
+						color: "#B88E2F",
+						fontSize: "24px",
+						fontWeight: "700",
+						lineHeight: "36px",
+						// textAlign:"left"
+					}}
+				>
+					Rs. 250,000.00
+				</Typography>
+			</div>
+			<Divider />
+
+			<div style={{ marginTop: "20px" }}>
+				<div style={{ display: "flex", gap: "10px", marginBottom: "10px" }}>
+					<img
+						src={Elipse}
+						alt="logo"
+						style={{
+							width: "12px",
+							height: "12px",
+						}}
+					/>
+					<Typography
+						variant="h4"
+						style={{
+							fontSize: "16px",
+							fontWeight: "400",
+							lineHeight: " 24px",
+						}}
+					>
+						{" "}
+						Direct Bank Transfer
+					</Typography>
+				</div>
+
+				<p style={{ color: "#9F9F9F" }}>
+					{" "}
+					Make your payment directly into our bank account. Please use <br />
+					your Order ID as payment reference. Your order will not be <br />
+					shipped until the funds have cleared in our account.
+				</p>
 			</div>
 
-             <p> Make your payment directly into our bank account. Please use <br/>
-             your Order ID as payment reference. Your order will not be <br/>
-             shipped until the funds have cleared in our account.
-             </p>
-
-
-             <div style={{ display: "flex", gap: "10px",marginBottom: "20px" }}>
- 				<img
- 					src={Elipse2}
- 					alt="logo"
- 					style={{
- 						width:"14px",
- 						height:"14px",
- 					}}
- 				/>
- 				<div>
- 					<h1
- 						style={{
- 							// fontSize: "24px",
- 							// fontWeight: "500",
- 							// lineHeight: " 36px",
-                            }}>
- 						{" "}
- 					Direct Bank Transfer
- 					</h1>
-			
+			{/* <div style={{ display: "flex", gap: "10px", marginBottom: "20px" }}>
+				<img
+					src={Elipse2}
+					alt="logo"
+					style={{
+						width: "14px",
+						height: "14px",
+					}}
+				/>
+				<div>
+					<h1
+						style={{
+							// fontSize: "24px",
+							// fontWeight: "500",
+							// lineHeight: " 36px",
+							color: "#9F9F9F",
+						}}
+					>
+						{" "}
+						Direct Bank Transfer
+					</h1>
 				</div>
- 			</div>
+			</div> */}
+			<div>
+				<FormControlLabel
+					value="other"
+					control={<Radio />}
+					label="Direct Bank Transfer"
+					sx={{ color: "#9F9F9F" }}
+				/>
+			</div>
 
-             <div style={{ display: "flex", gap: "10px",marginBottom: "20px" }}>
- 				<img
- 					src={Elipse2}
- 					alt="logo"
- 					style={{
- 						width: " 14px",
- 						height: "14px",
- 					}}
- 				/>
- 				<div>
- 					<h1
- 						style={{
- 							// fontSize: "24px",
- 							// fontWeight: "500",
- 							// lineHeight: " 36px",
-                            }}>
- 						{" "}
- 					Cash on Delivery
- 					</h1>
-			
- 				</div>
- 			</div>
-            
-             <p>Your personal data will be used to support your experience <br/>
-             throughout this website, to manage access to your account and for other purposes described in our <b>privacy policy.</b>
-              </p>
+			<div>
+				<FormControlLabel
+					value="other"
+					control={<Radio />}
+					label="Cash On Delivery"
+					sx={{ color: "#9F9F9F" }}
+				/>
+			</div>
 
-              <Stack direction="row" sx={{ justifyContent: 'center', alignItems: 'center', marginTop:"20px" }}>
- 							<Button
- 								// size="large"
- 								type="submit"
- 								sx={{ color: "black", 
-                                 width: "318px",
-                                 height: "64px",
-                                 borderRadius:"15px",
-                                border: "1px solid black"
-                             }}
- 							>
- 								Place holder
- 							</Button>
- 						</Stack>
-    </Box>
-    
+			<p>
+				Your personal data will be used to support your experience <br />
+				throughout this website, to manage access to your account and for other
+				purposes described in our <b>privacy policy.</b>
+			</p>
 
-  )
+			<Stack
+				direction="row"
+				sx={{
+					justifyContent: "center",
+					alignItems: "center",
+					marginTop: "20px",
+				}}
+			>
+				<Button
+					// size="large"
+					type="submit"
+					sx={{
+						color: "black",
+						width: "318px",
+						height: "64px",
+						borderRadius: "15px",
+						border: "1px solid black",
+					}}
+				>
+					Place holder
+				</Button>
+			</Stack>
+		</Box>
+	)
 }
-
-
-
-
-
-
