a2439fc0c455748526435426144d1ea66a66abf0
PriscillaAkumu <sulepriscilla@gmail.com>
2023-11-25 20:15:51 +0300

    about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
-import Banner from '../../components/Banner/Banner'
+import Banner from '../../components/Banner/Banner';
+import Profile from './Profile';
 
 const About = () => {
     const breadcrumbItems = [
@@ -14,6 +15,7 @@ const About = () => {
      pageTitle="About"
      breadcrumbItems={breadcrumbItems}
     />
+    <Profile/>
     </>
   );
 }
