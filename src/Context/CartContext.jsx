82ff05f3d21979281045351a11f2f0d3e35d0f11
Priscilla Akumu <105222371+PriscillaAkumu@users.noreply.github.com>
2023-11-26 00:12:08 +0300

    modified CartContext

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -2,52 +2,63 @@ import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
-const CartProvider = ({children}) => {
-const [cart, setCart] = useState([]) ; 
-// const addToCart = (product, id) => {
-//     console.log(product);
-//     console.log(`${product.name} added to cart`);
-// } 
-const addToCart = (product, selectedSize, selectedColor, quantity) => {
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    // Load cart from localStorage on component mount
+    const storedCart = JSON.parse(localStorage.getItem('cart'));
+    if (storedCart) {
+      setCart(storedCart);
+    }
+  }, []);
+
+  const addToCart = (product, selectedSize, selectedColor, quantity) => {
     const itemInCart = cart.find(
       (item) =>
         item.product.id === product.id &&
         item.selectedSize === selectedSize &&
         item.selectedColor === selectedColor
-        
     );
+
     if (itemInCart) {
-        // If the item is already in the cart, update the quantity
-        const updatedCart = cart.map((item) => {
-          if (
-            item.product.id === product.id &&
-            item.selectedSize === selectedSize &&
-            item.selectedColor === selectedColor
-          ) {
-            return {
-              ...item,
-              quantity: item.quantity + quantity,
-            };
-          } else {
-            return item;
+      // If the item is already in the cart, update the quantity
+      const updatedCart = cart.map((item) => {
+        if (
+          item.product.id === product.id &&
+          item.selectedSize === selectedSize &&
+          item.selectedColor === selectedColor
+        ) {
+          return {
+            ...item,
+            quantity: item.quantity + quantity,
+          };
+        } else {
+          return item;
         }
-    });
-    setCart(updatedCart);
-  } else {
-    // If the item is not in the cart, add it
-    setCart([
-      ...cart,
-      {
+      });
+      setCart(updatedCart);
+
+      // Use the updatedCart directly when storing in localStorage
+      localStorage.setItem('cart', JSON.stringify(updatedCart));
+    } else {
+      // If the item is not in the cart, add it
+      const newCartItem = {
         product: product,
         selectedSize: selectedSize,
         selectedColor: selectedColor,
         quantity: quantity,
-      },
-    ]);
-  }
-};
+      };
 
-const removeFromCart = (itemToRemove) => {
+      // Update the cart state and use the callback form to ensure the correct order of operations
+      setCart((prevCart) => [...prevCart, newCartItem]);
+
+      // Use the updatedCart directly when storing in localStorage
+      localStorage.setItem('cart', JSON.stringify([...cart, newCartItem]));
+    }
+  };
+
+  const removeFromCart = (itemToRemove) => {
     const updatedCart = cart.filter(
       (item) =>
         !(
@@ -57,14 +68,22 @@ const removeFromCart = (itemToRemove) => {
         )
     );
     setCart(updatedCart);
-  };
-return(
-<CartContext.Provider value={{cart, addToCart, removeFromCart}}>
-{children}
-</CartContext.Provider>
-)
 
+    // Use the updatedCart directly when storing in localStorage
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
+
+  
+      
+
+
 
